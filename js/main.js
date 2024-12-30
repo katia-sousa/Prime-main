804219c3e10c0c36dee9f8ea87bfed4b1f2578a7
katia-sousa <katia.ribeiro@qintess.com>
2024-12-30 17:20:20 -0300

    mensagem que informa que os dados foram salvos

--- js/main.js
+++ js/main.js
@@ -13,30 +13,50 @@ const email = document.getElementById('email')
 const telefone = document.getElementById('telefone')
 const btnSubscribe = document.getElementById('btnSubscribe')
 
-btnSubscribe.addEventListener('click', async () =>{
-  const subscription = {
-    cep: cep.value,
-    logradouro: logradouro.value,
-    numero: numero.value,
-    uf: uf.value,
-    bairro: bairro.value,
-    localidade: localidade.value,
-    valorContaLuz: valorContaLuz.value,
-    email: email.value,
-    telefone: telefone.value
-  }
-  //salvar
-  const formId = await form(subscription)
+btnSubscribe.addEventListener('click', async () => {
+    const subscription = {
+      cep: cep.value,
+      logradouro: logradouro.value,
+      numero: numero.value,
+      uf: uf.value,
+      bairro: bairro.value,
+      localidade: localidade.value,
+      valorContaLuz: valorContaLuz.value,
+      email: email.value,
+      telefone: telefone.value
+    };
+  
+    try {
+      // Salvar
+      const formId = await form(subscription);
+  
+      // Limpar os campos do formulário
+      cep.value = '';
+      logradouro.value = '';
+      numero.value = '';
+      uf.value = '';
+      bairro.value = '';
+      localidade.value = '';
+      valorContaLuz.value = '';
+      email.value = '';
+      telefone.value = '';
 
-    cep.value = ''
-    logradouro.value = ''
-    numero.value = ''
-    uf.value = ''
-    bairro.value = ''
-    localidade.value = ''
-    valorContaLuz.value = ''
-    email.value = ''
-    telefone.value = ''
-});
+        //alert
+      alert('Dados salvos com sucesso!');
+      
+      // Exibir mensagem de sucesso
+      const successMessage = document.getElementById('successMessage');
+      successMessage.style.display = 'block';
+  
+      // Ocultar a mensagem após alguns segundos
+      setTimeout(() => {
+        successMessage.style.display = 'none';
+      }, 3000); // Oculta após 3 segundos
+    } catch (error) {
+      console.error('Erro ao salvar os dados:', error);
+      alert('Ocorreu um erro ao salvar os dados. Tente novamente.');
+    }
+  });
+  
 //debbuger
 
